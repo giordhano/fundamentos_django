11f8bde5b3c40316679a05e0cef64a713c714f0a
Giordhano <giordhanovaldez@gmail.com>
2015-05-27 01:42:16 -0500

    Template listo

--- template_reusable/template_reusable.py
+++ template_reusable/template_reusable.py
@@ -1,13 +1,22 @@
 
+import os
 import sys
 
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG','on')  == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY','{{ secret_key }}')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost').split(',')
+
 settings.configure(
 
-    DEBUG = True,
+    DEBUG = DEBUG,
 
-    SECRET_KEY = 'holasoygiordhano',
+    SECRET_KEY = SECRET_KEY,
+
+    ALLOWED_HOSTS = ALLOWED_HOSTS,
 
     ROOT_URLCONF = __name__,
 
