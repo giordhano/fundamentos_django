e57c722fb221087faec2f9e924e5c26d17c78e71
Giordhano <giordhanovaldez@gmail.com>
2015-05-27 13:35:00 -0500

    Generador de imagen png listo

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -37,12 +37,24 @@ from django import forms
 from django.conf.urls import url
 from django.http import HttpResponse,HttpResponseBadRequest
 
+from io import BytesIO
+from PIL import Image
+
 
 class ImageForm(forms.Form):
     """ Form para validar los datos recibidos """
     height = forms.IntegerField(min_value = 1, max_value = 2000)
     width = forms.IntegerField(min_value=1, max_value =2000)
 
+    def generate(self, image_format = 'PNG'):
+
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB',(width,height))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
 
 
 def placeholder(request, width, height):
@@ -50,11 +62,11 @@ def placeholder(request, width, height):
     form = ImageForm({'height' : height, 'width' : width })
 
     if form.is_valid():
-        height = form.cleaned_data['height']
-        width = form.cleaned_data['width']
-        return HttpResponse('Ok')
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
     else:
         return HttpResponseBadRequest('Algo no esta bien') # Envia error 404
+
 
 def index(request):
     return HttpResponse('Hola estoy vivo')
