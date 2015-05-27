d12a9e552af31a06a0432770e2812136720aca2c
Giordhano <giordhanovaldez@gmail.com>
2015-05-27 02:40:47 -0500

    Proyecto generado en base al template creado

---
+++ placeholder/placeholder.py
@@ -0,0 +1,54 @@
+# django-admin.py startproject placeholder --template=template_reusable
+
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG','on')  == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY','2%&6s(o0*e$wd)@viy(!7fttu0tm&y3b7db4-wr3h!9nstxaw=')
+
+print(SECRET_KEY)
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost').split(',')
+
+settings.configure(
+
+    DEBUG = DEBUG,
+
+    SECRET_KEY = SECRET_KEY,
+
+    ALLOWED_HOSTS = ALLOWED_HOSTS,
+
+    ROOT_URLCONF = __name__,
+
+    MIDDLEWARE_CLASSES = (
+
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    ),
+
+)
+
+
+from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hola estoy vivo')
+
+
+urlpatterns = (
+
+    url(r'^$',index),
+
+)
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
